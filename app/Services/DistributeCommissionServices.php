df537cf5c345d8b938b3837abe445437e0f710a0
mrpranto <prantoabir1@gmail.com>
2021-08-15 11:01:11 +0600

    wip

--- app/Services/DistributeCommissionServices.php
+++ app/Services/DistributeCommissionServices.php
@@ -18,8 +18,25 @@ class DistributeCommissionServices extends BaseServices
 
     public function distributeCommission($user)
     {
-        foreach ($this->parentIds($user) as $key => $id){
-            $this->setCommission($this->getCommission()[$key], $id);
+        foreach ($this->parentIds($user) as $key => $id) {
+
+            if ($key == 4 && $this->countAffiliator($id)->count() < $this->getCommission()[0]['level_minimum_member']) {
+
+//                $this->setCommission($this->getCommission()[$key], $id);
+
+            } elseif (($key == 5 || $key == 6) && $this->secondLevelAffiliator($id)->count() < $this->getCommission()[1]['level_minimum_member']) {
+
+//                $this->setCommission($this->getCommission()[$key], $id);
+
+            } elseif (($key == 7 || $key == 8 || $key == 9) && $this->thirdLevelAffiliator($id)->count() < $this->getCommission()[2]['level_minimum_member']) {
+
+//                $this->setCommission($this->getCommission()[$key], $id);
+
+            } else {
+
+                $this->setCommission($this->getCommission()[$key], $id);
+
+            }
         }
     }
 
@@ -48,42 +65,42 @@ class DistributeCommissionServices extends BaseServices
             array_push($levelUserIds, $userParent->id);
             $hisParent = User::query()->where('id', $userParent->sponsor_user_id)->first();
 
-            if ($hisParent){
+            if ($hisParent) {
                 array_push($levelUserIds, $hisParent->id);
                 $hisParent2 = User::query()->where('id', $hisParent->sponsor_user_id)->first();
 
-                if ($hisParent2){
+                if ($hisParent2) {
                     array_push($levelUserIds, $hisParent2->id);
                     $hisParent3 = User::query()->where('id', $hisParent2->sponsor_user_id)->first();
 
-                    if ($hisParent3){
+                    if ($hisParent3) {
                         array_push($levelUserIds, $hisParent3->id);
                         $hisParent4 = User::query()->where('id', $hisParent3->sponsor_user_id)->first();
 
-                        if ($hisParent4){
+                        if ($hisParent4) {
 
                             array_push($levelUserIds, $hisParent4->id);
                             $hisParent5 = User::query()->where('id', $hisParent4->sponsor_user_id)->first();
 
-                            if ($hisParent5){
+                            if ($hisParent5) {
                                 array_push($levelUserIds, $hisParent5->id);
                                 $hisParent6 = User::query()->where('id', $hisParent5->sponsor_user_id)->first();
 
-                                if ($hisParent6){
+                                if ($hisParent6) {
                                     array_push($levelUserIds, $hisParent6->id);
                                     $hisParent7 = User::query()->where('id', $hisParent6->sponsor_user_id)->first();
 
-                                    if ($hisParent7){
+                                    if ($hisParent7) {
 
                                         array_push($levelUserIds, $hisParent7->id);
                                         $hisParent8 = User::query()->where('id', $hisParent7->sponsor_user_id)->first();
 
-                                        if ($hisParent8){
+                                        if ($hisParent8) {
 
                                             array_push($levelUserIds, $hisParent8->id);
                                             $hisParent9 = User::query()->where('id', $hisParent8->sponsor_user_id)->first();
 
-                                            if ($hisParent9){
+                                            if ($hisParent9) {
 
                                                 array_push($levelUserIds, $hisParent9->id);
                                                 $hisParent10 = User::query()->where('id', $hisParent9->sponsor_user_id)->first();
@@ -112,6 +129,22 @@ class DistributeCommissionServices extends BaseServices
         }
 
         return $levelUserIds;
+    }
+
+
+    public function countAffiliator($id)
+    {
+        return $this->user->newQuery()->where('sponsor_user_id', $id)->get(['id', 'name']);
+    }
+
+    public function secondLevelAffiliator($id)
+    {
+        return $this->user->newQuery()->whereIn('sponsor_user_id', $this->countAffiliator($id)->pluck('id'))->get(['id', 'name']);
+    }
+
+    public function thirdLevelAffiliator($id)
+    {
+        return $this->user->newQuery()->whereIn('sponsor_user_id', $this->secondLevelAffiliator($id)->pluck('id'))->get(['id', 'name']);
     }
 
 
